5da4090a7d348eaa1e65398c3a5dbfe5c692884f
MarinZhang <1475705241@qq.com>
2021-03-21 12:45:10 +0800

    学习jsonp的使用

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+//import axios from 'axios'
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
@@ -15,8 +16,16 @@ export default {
   },
   data(){
     return {
-      age: 30
+      age: 30,
+      data: ''
     }
+  },
+  mounted(){
+    let url = "https://coding.imooc.com/index/getadverhttps://www.imooc.com/search/hotwords";
+    jsonp(url, (err,res)=>{
+      let result = res;
+      this.data = result;
+    })
   }
 }
 </script>
