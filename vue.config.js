d6eb857bed2e4c627c42279ee987b8c4c0b57a42
MarinZhang <1475705241@qq.com>
2021-03-22 22:30:15 +0800

    项目目录结构设置

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,15 @@
-module.exports = {}
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'https://www.imooc.com',
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        }
+    }
+}
