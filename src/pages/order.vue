cead9de73045172da24dcd546dbd763bf69a4d92
MarinZhang <1475705241@qq.com>
2021-03-27 23:18:07 +0800

    路由封装

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <nav-header></nav-header>
+    <order-header></order-header>
     <router-view></router-view>
     <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
-import NavHeader from "@/components/NavHeader";
+import OrderHeader from "@/components/OrderHeader";
 import NavFooter from "@/components/NavFooter";
 export default {
-  name: 'home',
-  components: {NavFooter, NavHeader}
+  name: 'order',
+  components: {
+    OrderHeader: OrderHeader,
+    NavFooter: NavFooter
+  }
 }
 </script>
