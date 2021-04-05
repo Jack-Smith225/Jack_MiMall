cb986a8e1c28612e79c5030f1bda8bfbfb8f8847
MarinZhang <1475705241@qq.com>
2021-04-05 14:10:17 +0800

    nav-header组件(2)

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -20,7 +20,32 @@
     </div>
     <!--/*顶部条下面的导航*/-->
     <div class="nav-header">
+      <div class="container">
+        <div class="header-logo">
+          <a href="/#/index"></a>
+        </div>
+        <div class="header-menu">
+           <div class="item-menu">
+             <span>小米手机</span>
+             <div class="children">
 
+             </div>
+           </div>
+          <div class="item-menu">
+            <span>RedMid红米</span>
+            <div class="children">
+
+            </div>
+          </div>
+          <div class="item-menu">
+            <span>电视</span>
+            <div class="children">
+
+            </div>
+          </div>
+        </div>
+        <div class="header-search"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,7 +57,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import "../assets/scss/base";
 .header {
   .nav-topbar {
     height: 39px;
@@ -42,10 +67,7 @@ export default {
     color: #B0B0B0;
 
     .container {
-      width: 1226px;
-      /*让container居中*/
-      margin-right: auto;
-      margin-left: auto;
+      /*base.scss -> container*/
       display: flex;
       justify-content: space-between; /*两边对齐*/
       align-items: center; /*定义项目在交叉轴上如何对齐*/
@@ -66,6 +88,69 @@ export default {
           background: url('/imgs/icon-cart-checked.png') no-repeat center;
           background-size: contain; /*缩放背景图片以完全装入背景区，可能背景区部分空白*/
           margin-right: 4px;
+        }
+      }
+    }
+  }
+  .nav-header {
+    .container {
+      /*base.scss -> container*/
+      height: 112px;
+      display: flex;
+      justify-content: space-between; /*两端对齐*/
+      align-items: center; /*在交叉轴的维度*/
+      .header-logo {
+        display: inline-block;
+        width: 55px;
+        height: 55px;
+        background-color: #FF6600;
+
+        a {
+          display: inline-block;
+          width: 110px;
+          height: 55px;
+
+          &:before { /*scss的伪类*/
+            content: ' '; /*content是占位的, 如果不加, 伪类就无法正常显示*/
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url("/imgs/mi-logo.png") no-repeat center;
+            background-size: 55px;
+            transition: margin 0.2s;
+          }
+
+          &:after { /*scss的伪类*/
+            content: ' ';
+            display: inline-block;
+            width: 55px;
+            height: 55px;
+            background: url("/imgs/mi-home.png") no-repeat center;
+            background-size: 55px;
+          }
+
+          &:hover:before {
+            margin-left: -55px;
+            transition: margin 0.2s;
+          }
+        }
+      }
+      .header-menu{
+        display: inline-block;
+        width: 200px;
+        padding-left: 209px;
+        .item-menu {
+          display: inline-block;
+          color: #333333;
+          font-weight: bold;
+          font-size: 16px;
+          line-height: 112px;
+          span {
+            cursor: pointer;
+          }
+          &:hover{ /*控制item-menu的hover*/
+
+          }
         }
       }
     }
