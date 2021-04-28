046430eee0e8390da1cb8802af58a229182ee2d7
MarinZhang <1475705241@qq.com>
2021-04-28 13:01:33 +0800

    10-2-(1) 购物车列表渲染=>静态部分

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -8,18 +8,22 @@
               <h2>{{ title }}<slot name="tip"></slot></h2>
           </div>
           <div class="username">
-              <a href="javascript:;">Jack</a>
+              <a href="javascript:;">{{ this.username }}</a>
           </div>
       </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 export default {
   name: 'order-header',
     props:{
       /*复用的名称*/
       title: String,
+    },
+    computed:{
+      ...mapState(['username'])
     }
 }
 </script>
