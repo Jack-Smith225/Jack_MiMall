87cc9f9a98eb10285480efcb360686df75f7248d
MarinZhang <1475705241@qq.com>
2021-05-05 16:18:01 +0800

    12-5 新增地址交互实现

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -62,7 +62,7 @@
                                     </a>
                                 </div>
                             </div>
-                            <div class="addr-add">
+                            <div class="addr-add" @click="openAddressModal">
                                 <div class="icon-add"></div>
                                 <div>添加新地址</div>
                             </div>
@@ -127,21 +127,21 @@
             <template v-slot:body>
                 <div class="edit-wrap"> <!--四行编辑框-->
                     <div class="item">
-                        <input type="text" class="input" placeholder="姓名">
-                        <input type="text" class="input" placeholder="手机号">
+                        <input type="text" class="input" placeholder="姓名" v-model="checkedItem.receiverName">
+                        <input type="text" class="input" placeholder="手机号" v-model="checkedItem.receiverMobile">
                     </div>
                     <div class="item">
-                        <select name="province">
+                        <select name="province" v-model="checkedItem.receiverProvince">
                             <option value="北京">北京</option>
                             <option value="天津">天津</option>
                             <option value="河北">河北</option>
                         </select>
-                        <select name="city">
+                        <select name="city" v-model="checkedItem.receiverCity">
                             <option value="北京">北京</option>
                             <option value="天津">天津</option>
                             <option value="河北">石家庄</option>
                         </select>
-                        <select name="district">
+                        <select name="district" v-model="checkedItem.receiverDistrict">
                             <option value="昌平">昌平区</option>
                             <option value="天津">海淀区</option>
                             <option value="河北">东城区</option>
@@ -151,10 +151,10 @@
                         </select>
                     </div>
                     <div class="item">
-                        <textarea name="street" placeholder="详细地址"></textarea>
+                        <textarea name="street" placeholder="详细地址" v-model="checkedItem.receiverAddress"></textarea>
                     </div>
                     <div class="item">
-                        <input type="text" class="input" placeholder="邮编">
+                        <input type="text" class="input" placeholder="邮编" v-model="checkedItem.receiverZip">
                     </div>
                 </div>
             </template>
@@ -185,7 +185,7 @@ export default {
             checkedItem: {}, //选中的商品对象
             userAction: '', // 用户行为: 0:新增 1:编辑 2:删除
             showDelModal: false,
-            showEditModal:true, //是否显示新增/编辑弹框
+            showEditModal: false, //是否显示新增/编辑弹框
         };
     },
     mounted() {
@@ -212,6 +212,12 @@ export default {
                 });
             });
         },
+        //打开
+        openAddressModal() {
+            this.userAction = 0; // 0是新增
+            this.checkedItem = {};
+            this.showEditModal = true;
+        },
         //
         delAddress(item) {
             this.checkedItem = item;
@@ -221,7 +227,7 @@ export default {
         // 地址删除,编辑,新增功能
         submitAddress() {
             let {checkedItem, userAction} = this;
-            let method, url;
+            let method, url, params = {}; // params专门为添加和编辑使用
             if (userAction == 0) {
                 method = 'post', url = '/shippings';
             } else if (userAction == 1) {
@@ -230,7 +236,43 @@ export default {
                 method = 'delete', url = `/shippings/${checkedItem.id}`;
             }
 
-            this.axios[method](url).then(
+            if (userAction == 0 || userAction == 1) {
+
+                let {receiverName, receiverMobile, receiverProvince, receiverCity, receiverDistrict,
+                    receiverAddress, receiverZip} = checkedItem;
+
+                let errMsg;
+                if (!receiverName) {
+                    errMsg = "请输入收货人名称";
+                }else if (!receiverMobile || !/\d{11}/.test(receiverMobile)) {
+                    errMsg = "请输入正确的手机号";
+                }else if (!receiverProvince) {
+                    errMsg = "请选择省份";
+                }else if (!receiverCity ) {
+                    errMsg = "请选择城市";
+                }else if (!receiverDistrict || !receiverAddress) {
+                    errMsg = "请输入收货地址";
+                }else if ( !/\d{6}/.test(receiverZip)) {
+                    errMsg = "请输入6位邮编";
+                }
+
+                if (errMsg) {
+                    this.$message.error(errMsg);
+                    return;
+                }
+
+                params = {
+                    receiverName,
+                    receiverMobile,
+                    receiverProvince,
+                    receiverCity,
+                    receiverDistrict,
+                    receiverAddress,
+                    receiverZip,
+                };
+            }
+
+            this.axios[method](url, params).then(
                 () => {
                     this.closeModal();
                     this.getAddressList();
@@ -244,7 +286,9 @@ export default {
             this.checkedItem = {};
             this.userAction = '';
             this.showDelModal = false;
-        }
+            this.showEditModal = false;
+        },
+
     }
 }
 </script>
@@ -433,8 +477,10 @@ export default {
 
     .edit-wrap {
         font-size: 14px;
+
         .item {
             margin-bottom: 15px;
+
             .input {
                 display: inline-block;
                 width: 283px;
@@ -444,16 +490,18 @@ export default {
                 border: 1px solid #e5e5e5;
 
                 // 指定兄弟元素的样式
-                &+.input{
+                & + .input {
                     margin-left: 14px;
                 }
             }
+
             select {
                 height: 40px;
                 line-height: 40px;
                 border: 1px solid #e5e5e5;
                 margin-right: 15px;
             }
+
             textarea {
                 height: 62px;
                 width: 100%;
