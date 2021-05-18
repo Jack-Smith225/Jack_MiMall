2da3f9190ac0bb6e4bda4839fbaa076e61611d56
MarinZhang <1475705241@qq.com>
2021-05-18 16:08:02 +0800

    15-2 vue.config配置介绍

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // start::本地开发服务器
   devServer: {
     host: 'localhost',
     port: 8080,
@@ -12,5 +13,15 @@ module.exports = {
       }
     }
   },
+  // end::本地开发服务器
+
+  // publicPath: '/', // 根路径, 前身是baseUrl, 示例可查看有道云笔记
+
+  // outputDir: 'dist', // 打包的输出路径
+
+  // indexPath: 'index.html', // 有默认选项: index.html
+
+  productionSourceMap: false, // 决定是否有 js.map文件
+
   // lintOnSave: false // 关闭eslint
-}
+};
